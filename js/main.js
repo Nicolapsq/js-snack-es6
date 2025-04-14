09fdcbdfd472c2584ced25c48d73acdf1aa05c3e
Nicolapsq <nicolapasqua84@gmail.com>
2025-04-14 18:51:32 +0200

    Add-loop-for

--- js/main.js
+++ js/main.js
@@ -1,22 +1,22 @@
-// // SNACK 1
+// SNACK 1
 
-// const listaBici = [
-//     {
-//         nome: "Specialized Tarmac",
-//         peso: "10 Kg",
-//     },
-//     {
-//         nome: "Trek Émonda",
-//         peso: "9 Kg",
-//     },
-//     {
-//         nome: "Cannondale SuperSix EVO",
-//         peso: "8 Kg",
-//     }
-// ]
-// // console.log(listaBici);
+const listaBici = [
+    {
+        nome: "Specialized Tarmac",
+        peso: "10 Kg",
+    },
+    {
+        nome: "Trek Émonda",
+        peso: "9 Kg",
+    },
+    {
+        nome: "Cannondale SuperSix EVO",
+        peso: "8 Kg",
+    }
+]
+console.log(listaBici);
 
-// //   Trovare la bici con peso piu basso:
+//   Trovare la bici con peso piu basso:
 // const filtroPesoMinore = listaBici.reduce(function(min, bici) {
 //     const pesoBici = parseFloat(bici.peso);
 //     const pesoMin = parseFloat(min.peso);
@@ -31,58 +31,76 @@
 
 // Versione alternativa con for:
 
+let pesoMin = listaBici[0];
 
-
-// SNACK 2
-
-//
-// FUNCTIONS /////////////////////////
-//
-
-// Genero Numeri Random:
-function numeriRandomDa1A50() {
-    const numeroGenerato = Math.floor(Math.random() * 50) + 1;
-    return numeroGenerato;
-}
-
-////////////////////////////////////////
-
-// array di oggetti
-
-const squadreCalcio = [
-    {
-        nome: "Juventus",
-        puntiFatti: "0",
-        falliSubiti: "0",
-    },
-    {
-        nome: "Inter ",
-        puntiFatti: "0",
-        falliSubiti: "0",
-    },
-    {
-        nome: "AC Milan",
-        puntiFatti: "0",
-        falliSubiti: "0",
-    },
-    {
-        nome: "AS Roma",
-        puntiFatti: "0",
-        falliSubiti: "0",
-    },
-    {
-        nome: "Napoli",
-        puntiFatti: "0",
-        falliSubiti: "0",
+for (let i = 0; i < listaBici.length; i++) {
+    const pesoBici = parseFloat(listaBici[i].peso);
+    const pesoBiciMin = parseFloat(pesoMin.peso);
+    if (pesoBici < pesoBiciMin) {
+        pesoMin = listaBici[i];
     }
-]
+}
+console.log(pesoMin);
 
-// console.log(numero);
-let numeroRandom = numeriRandomDa1A50();
+// // SNACK 2
 
-// Modifico i valori delle chiavi puntiFatti e falliSubiti inserendo un numero random:
-squadreCalcio.forEach(function(mod){
-    mod.puntiFatti = numeriRandomDa1A50();
-    mod.falliSubiti = numeriRandomDa1A50();
-})
-console.log(squadreCalcio);
+// //
+// // FUNCTIONS /////////////////////////
+// //
+
+// // Genero Numeri Random:
+// function numeriRandomDa1A50() {
+//     const numeroGenerato = Math.floor(Math.random() * 50) + 1;
+//     return numeroGenerato;
+// }
+
+// ////////////////////////////////////////
+
+// // array di oggetti
+
+// const squadreCalcio = [
+//     {
+//         nome: "Juventus",
+//         puntiFatti: "0",
+//         falliSubiti: "0",
+//     },
+//     {
+//         nome: "Inter ",
+//         puntiFatti: "0",
+//         falliSubiti: "0",
+//     },
+//     {
+//         nome: "AC Milan",
+//         puntiFatti: "0",
+//         falliSubiti: "0",
+//     },
+//     {
+//         nome: "AS Roma",
+//         puntiFatti: "0",
+//         falliSubiti: "0",
+//     },
+//     {
+//         nome: "Napoli",
+//         puntiFatti: "0",
+//         falliSubiti: "0",
+//     }
+// ]
+
+// // console.log(numero);
+// let numeroRandom = numeriRandomDa1A50();
+
+// // Modifico i valori delle chiavi puntiFatti e falliSubiti inserendo un numero random:
+// squadreCalcio.forEach(function(mod){
+//     mod.puntiFatti = numeriRandomDa1A50();
+//     mod.falliSubiti = numeriRandomDa1A50();
+// })
+// console.log(squadreCalcio);
+
+// // Creo una nuova lista di oggetti modificata rispetto all'originale:
+// const squadreCalcioMod = squadreCalcio.map(function(squadra){
+//     return{
+//         nome: squadra.nome,
+//         falliSubiti: squadra.falliSubiti
+//     }
+// });
+// console.log(squadreCalcioMod);
